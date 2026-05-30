2fae23e06e65855e13546ed14456d50eecf4b1f2
Shweta Burgula <shweta.burgula@gmail.com>
2026-05-30 15:33:33 +0100

    feat: add camera connectivity test

---
+++ src/picam/camera_test.py
@@ -0,0 +1,14 @@
+from picamera2 import Picamera2
+import time
+
+camera = Picamera2()
+
+camera.start()
+
+time.sleep(2)
+
+camera.capture_file("test.jpg")
+
+print("Saved test.jpg")
+
+camera.stop()
